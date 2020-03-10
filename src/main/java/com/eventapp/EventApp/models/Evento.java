6b6df10f0f9a67f757c258c7e16960f3afc60f27
Rafael <rafaelhop@hotmail.com>
2020-03-10 19:58:15 -0300

    Fix identation

--- src/main/java/com/eventapp/EventApp/models/Evento.java
+++ src/main/java/com/eventapp/EventApp/models/Evento.java
@@ -7,8 +7,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import org.hibernate.validator.constraints.NotEmpty;
-
 
 @Entity
 public class Evento implements Serializable{
