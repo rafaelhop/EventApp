d5f27c2869f98a28bc3807aeb871e38f7c611092
Rafael <rafaelhop@hotmail.com>
2020-03-09 22:05:43 -0300

    Add database configuration

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -1,10 +1,30 @@
 package models;
 
-public class Event {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Event implements Serializable {
+	
+	private static final long serialVersionUID = 5296331219443839896L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
 	private String name;
 	private String local;
 	private String data;
 	private String horary;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getName() {
 		return name;
