9d19268b8ba913dedbd772d92dc5c3ffecc0be3e
Rafael <rafaelhop@hotmail.com>
2020-03-10 19:21:57 -0300

    Add method list events

--- src/main/java/com/eventapp/EventApp/controllers/EventController.java
+++ src/main/java/com/eventapp/EventApp/controllers/EventController.java
@@ -4,24 +4,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.eventapp.EventApp.models.Evento;
 import com.eventapp.EventApp.repository.EventRepository;
 
 @Controller
 public class EventController {
-	
+
 	@Autowired
 	private EventRepository eventRepository;
-	
-	@RequestMapping(value ="cadastrarEvento", method = RequestMethod.GET)
+
+	@RequestMapping(value = "cadastrarEvento", method = RequestMethod.GET)
 	public String form() {
-		return"event/formEvent";
+		return "event/formEvent";
 	}
-	@RequestMapping(value ="cadastrarEvento", method = RequestMethod.POST)
+
+	@RequestMapping(value = "cadastrarEvento", method = RequestMethod.POST)
 	public String form(Evento evento) {
 		eventRepository.save(evento);
-		return"redirect:/cadastrarEvento";
+		return "redirect:/cadastrarEvento";
+	}
+
+	@RequestMapping("eventos")
+	public ModelAndView listaEventos() {
+		ModelAndView view = new ModelAndView("index");
+		Iterable<Evento> eventos = eventRepository.findAll();
+		view.addObject("eventos", eventos);
+		return view;
 	}
 
 }
