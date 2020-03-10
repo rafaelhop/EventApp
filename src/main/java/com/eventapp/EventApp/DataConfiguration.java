de96f7634db2127b38358eaffbc1422d521c3faf
Rafael <rafaelhop@hotmail.com>
2020-03-10 14:39:06 -0300

    Fix

--- src/main/java/com/eventapp/EventApp/DataConfiguration.java
+++ src/main/java/com/eventapp/EventApp/DataConfiguration.java
@@ -15,7 +15,7 @@ public class DataConfiguration {
 	@Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
 		dataSource.setUrl("jdbc:mysql://localhost:3306/eventapp");
 		dataSource.setUsername("root");
 		dataSource.setPassword("");
@@ -27,7 +27,7 @@ public class DataConfiguration {
 		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
 		adapter.setDatabase(Database.MYSQL);
 		adapter.setShowSql(true);
-		adapter.setGenerateDdl(true);
+		adapter.setGenerateDdl(false);
 		adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
 		adapter.setPrepareConnection(true);
 		return adapter;
