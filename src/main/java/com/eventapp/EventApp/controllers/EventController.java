e35b5864540c12ae018645a9e8f80031be3cf7bb
Rafael <rafaelhop@hotmail.com>
2020-03-19 19:07:05 -0300

    Add method view for events

--- src/main/java/com/eventapp/EventApp/controllers/EventController.java
+++ src/main/java/com/eventapp/EventApp/controllers/EventController.java
@@ -2,6 +2,7 @@ package com.eventapp.EventApp.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -32,6 +33,15 @@ public class EventController {
 		Iterable<Evento> eventos = eventRepository.findAll();
 		view.addObject("eventos", eventos);
 		return view;
+	}
+
+	@RequestMapping("view/{codigo}")
+	public ModelAndView viewEventos(@PathVariable("codigo") Long codigo) {
+		ModelAndView view = new ModelAndView("event/detalhesEventos");
+		Evento evento = eventRepository.findByCodigo(codigo);
+		view.addObject("evento", evento);
+		return view;
+
 	}
 
 }
